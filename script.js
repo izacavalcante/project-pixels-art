bbe4851e8e83be3284af3c4f24700a588da1f483
Izabella Cristina <oliveira_cavalcante_@hotmail.com>
2022-04-25 10:18:38 -0300

    Requisitos em desenvolvimento

---
+++ script.js
@@ -0,0 +1,9 @@
+// let colorBlack = document.querySelector("#color1");
+// colorBlack.className = "selected";
+
+let color1 = document.querySelector('#color1');
+let color2 = document.querySelector('#color2');
+let color3 = document.querySelector('#color3');
+let color4 = document.querySelector('#color4');
+
+color1.addEventListener('click', function());
