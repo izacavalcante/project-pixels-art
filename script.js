faffd58b0a03d35d7784815041bacc42041e3f11
Izabella Cristina <oliveira_cavalcante_@hotmail.com>
2022-04-25 10:40:38 -0300

    Requisito 6 adicionado

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-// let colorBlack = document.querySelector("#color1");
-// colorBlack.className = "selected";
+let colorBlack = document.querySelector("#color1");
+colorBlack.classList.add("selected");
 
 let color1 = document.querySelector('#color1');
 let color2 = document.querySelector('#color2');
 let color3 = document.querySelector('#color3');
 let color4 = document.querySelector('#color4');
 
-color1.addEventListener('click', function());
+color1.addEventListener('click', colorN1);
+function colorN1(){
+
+}
