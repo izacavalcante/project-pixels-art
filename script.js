22b2f9acbf0191523423a510081875c7a229c047
Izabella Cristina <oliveira_cavalcante_@hotmail.com>
2022-04-26 13:10:24 -0300

    Requisitos em desenvolvimento

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 let colorBlack = document.querySelector('#color1');
 colorBlack.classList.add('selected');
 
+function colores() {
+    const palette = document.querySelector('.color');
+    for (let i = 0; i < palette.lenght; i += 1) {
+        palette[i].addEventListener('click', function (event) {
+            const selected = document.querySelector('.selected')[0];
+            selected.classList.remove('selected');
+            event.target.classList.add('selected');
+        });
+    }
+}
+
+window.onload = () => {
+    colores();
+};
+
+// function mudaClasse (event) {
+//     let corSelecionada = document.querySelector('.selected');
+//     corSelecionada.classList.remove('selected');
+//     event.target.classList.add('selected');
+
+
+    
+// }
