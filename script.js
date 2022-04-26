52764926cd4d563045d30a434229ec3ffbf065e6
Izabella Cristina <oliveira_cavalcante_@hotmail.com>
2022-04-26 14:18:30 -0300

    Requisitos em desenvolimento

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 let colorBlack = document.querySelector('#color1');
 colorBlack.classList.add('selected');
 
-function colores() {
-    const palette = document.querySelector('.color');
-    for (let i = 0; i < palette.lenght; i += 1) {
-        palette[i].addEventListener('click', function (event) {
-            const selected = document.querySelector('.selected')[0];
-            selected.classList.remove('selected');
-            event.target.classList.add('selected');
-        });
-    }
-}
+// let colors = document.getElementsByClassName("color");
 
-window.onload = () => {
-    colores();
-};
-
-// function mudaClasse (event) {
-//     let corSelecionada = document.querySelector('.selected');
-//     corSelecionada.classList.remove('selected');
-//     event.target.classList.add('selected');
-
-
-    
+// function select(evento) {
+//     for (let index of colors ) {
+//     index.classList.remove("selected")
 // }
+
+//     evento.target.classList.add("selected")
+// }
+
+// for (let index of colors ) {
+//     index.addEventListener("click", select);
+// }
+
+// function colores() {
+//     const palette = document.querySelector('.color');
+//     for (let i = 0; i < palette.lenght; i += 1) {
+//         palette[i].addEventListener('click', function (event) {
+//             const selected = document.querySelector('.selected')[0];
+//             selected.classList.remove('selected')[i];
+//             event.target.classList.add('selected')[i];
+//         });
+//     }
+// }
+
+// window.onload = () => {
+//     colores();
+// };
