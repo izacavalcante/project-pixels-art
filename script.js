19b7fb1592eb1b5211e483eb04ccbb498945a1d0
Izabella Cristina <oliveira_cavalcante_@hotmail.com>
2022-04-26 19:44:23 -0300

    Requisito 7 adicionado

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
 let colorBlack = document.querySelector('#color1');
 colorBlack.classList.add('selected');
 
-// let colors = document.getElementsByClassName("color");
-
-// function select(evento) {
-//     for (let index of colors ) {
-//     index.classList.remove("selected")
-// }
-
-//     evento.target.classList.add("selected")
-// }
-
-// for (let index of colors ) {
-//     index.addEventListener("click", select);
-// }
-
-// function colores() {
-//     const palette = document.querySelector('.color');
-//     for (let i = 0; i < palette.lenght; i += 1) {
-//         palette[i].addEventListener('click', function (event) {
-//             const selected = document.querySelector('.selected')[0];
-//             selected.classList.remove('selected')[i];
-//             event.target.classList.add('selected')[i];
-//         });
+// let colores = document.querySelector('.color');
+// colores.addEventListener('click', function (){
+//     const selected = document.querySelector('.selected');
+//     selected.classList.remove('selected');
+//     // selected.classList.add('selected');
+// });
+// function colorPaletteClicked(event) {
+//     for (let color of colorPalette) {
+//         if (color.className === 'color selected') {
+//             color.className = 'color';
+//         }
 //     }
+//     event.target.className = 'color selected';
 // }
 
-// window.onload = () => {
-//     colores();
-// };
+const color1 = document.querySelector('#color1');
+const color2 = document.querySelector('#color2');
+const color3 = document.querySelector('#color3');
+const color4 = document.querySelector('#color4');
+
+color1.addEventListener('click', selectSelected);
+color2.addEventListener('click', selectSelected);
+color3.addEventListener('click', selectSelected);
+color4.addEventListener('click', selectSelected);
+
+function selectSelected(event) {
+    const selected = document.querySelector('.selected');
+    selected.classList.remove('selected');
+    event.target.classList.add('selected'); // O .target retorna o elemento que acionou o evento.
+}
