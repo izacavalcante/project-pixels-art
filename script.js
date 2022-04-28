d8462da64adc8f42efbccb9a0e37ad49ccaa79a7
Izabella Cristina <oliveira_cavalcante_@hotmail.com>
2022-04-28 16:56:01 -0300

    Requisito 8 adicionado

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 let colorBlack = document.querySelector('#color1');
 colorBlack.classList.add('selected');
 
-
 const color1 = document.querySelector('#color1');
 const color2 = document.querySelector('#color2');
 const color3 = document.querySelector('#color3');
 const color4 = document.querySelector('#color4');
-
 color1.addEventListener('click', selectSelected);
 color2.addEventListener('click', selectSelected);
 color3.addEventListener('click', selectSelected);
@@ -18,15 +16,16 @@ function selectSelected(event) {
     event.target.classList.add('selected'); // O .target retorna o elemento que acionou o evento.
 }
 
-
-let selected = document.querySelector('.selected');
-let pixels = document.querySelectorAll('.pixel');
-const cssSelected = window.getComputedStyle(selected, null);
-let bgColor = cssSelected.getPropertyValue("background-color");
-
-pixels.addEventListener('click', function(){
-    for (let i = 0; i < cssSelected.length; i += 1) {
-        cssSelected.style = cssSelected.item[i];
+function colorirPixels() {
+    let pixels = document.querySelectorAll('.pixel');
+    for (let i of pixels) {
+        i.addEventListener('click', function(event){
+            let selected = document.querySelector('.selected');
+            const cssSelected = window.getComputedStyle(selected, null);
+            let bgColor = cssSelected.getPropertyValue('background-color');
+            i.style.backgroundColor = bgColor;
+            console.log(event.target);
+        });
     }
-})
-console.log(cssSelected)
+}    
+colorirPixels();
