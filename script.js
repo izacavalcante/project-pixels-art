9c51f488a729d9c27500e7725742a13c80bb1c03
Izabella Cristina <oliveira_cavalcante_@hotmail.com>
2022-04-28 18:49:06 -0300

    Requisito 9 em desenvolvimento

--- script.js
+++ script.js
@@ -1,31 +1,40 @@
-let colorBlack = document.querySelector('#color1');
+const colorBlack = document.querySelector('#color1');
 colorBlack.classList.add('selected');
 
 const color1 = document.querySelector('#color1');
 const color2 = document.querySelector('#color2');
 const color3 = document.querySelector('#color3');
 const color4 = document.querySelector('#color4');
+
+function selectSelected(event) {
+  const selected = document.querySelector('.selected');
+  selected.classList.remove('selected');
+  event.target.classList.add('selected'); // O .target retorna o elemento que acionou o evento.
+}
+
 color1.addEventListener('click', selectSelected);
 color2.addEventListener('click', selectSelected);
 color3.addEventListener('click', selectSelected);
 color4.addEventListener('click', selectSelected);
 
-function selectSelected(event) {
-    const selected = document.querySelector('.selected');
-    selected.classList.remove('selected');
-    event.target.classList.add('selected'); // O .target retorna o elemento que acionou o evento.
-}
-
 function colorirPixels() {
-    let pixels = document.querySelectorAll('.pixel');
-    for (let i of pixels) {
-        i.addEventListener('click', function(event){
-            let selected = document.querySelector('.selected');
-            const cssSelected = window.getComputedStyle(selected, null);
-            let bgColor = cssSelected.getPropertyValue('background-color');
-            i.style.backgroundColor = bgColor;
-            console.log(event.target);
-        });
-    }
-}    
+  const pixels = document.querySelectorAll('.pixel');
+  for (let i of pixels) {
+    i.addEventListener('click', function(event) {
+      const selected = document.querySelector('.selected');
+      const cssSelected = window.getComputedStyle(selected, null);
+      const bgColor = cssSelected.getPropertyValue('background-color');
+       /* https:www.w3schools.com/jsref/jsref_getcomputedstyle.asp */
+      i.style.backgroundColor = bgColor;
+      console.log(event.target);
+    } );
+  }
+}
 colorirPixels();
+
+const body = document.querySelector('#button');
+const button = document.createElement('button');
+const textButton = 'Limpar';
+button.innerText = textButton;
+body.appendChild(button);
+button.setAttribute('id', 'clear-board');
